19b7d282d4536296be43406b4af78e2b081b83f9
LowkeyCoding <loke0046@gmail.com>
2025-02-27 11:28:35 +0100

    Added functionality to generate fresh names

---
+++ app/Fresh.hs
@@ -0,0 +1,17 @@
+module Fresh where
+
+import Control.Monad.State
+
+-- Define a type for generating fresh names
+type NameGenerator = State Int
+
+-- Function to generate a fresh name with a custom prefix
+freshName :: String -> NameGenerator String
+freshName prefix = do
+  counter <- get          -- Get the current counter value
+  put (counter + 1)       -- Increment the counter
+  return (prefix ++ show counter)  -- Return a fresh name like "prefix0", "prefix1", etc.
+
+-- Helper function to run the name generator
+runNameGenerator :: NameGenerator a -> a
+runNameGenerator generator = evalState generator 0  -- Start with counter = 0
